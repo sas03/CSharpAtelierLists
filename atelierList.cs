6ae7f33a3c737a9a1d9a5de2f2a71a34fd8f0bc1
sas03 <stephanesumo@yahoo.fr>
2023-06-19 16:21:18 +0200

    Access Modifiers

--- atelierList.cs
+++ atelierList.cs
@@ -7,11 +7,11 @@ class atelierList
         liste = new List<int>();
     }
 
-    public void ajoutElement(int e){
+    private void ajoutElement(int e){
         liste.Add(e);
         Console.WriteLine(e + " added");
     }
-    public void supprElement(){
+    private void supprElement(){
         while(true){
             Console.WriteLine("Type the id of the element to remove: ");
             int id = Convert.ToInt32(Console.ReadLine());
@@ -25,15 +25,15 @@ class atelierList
             }
         } 
     }
-    public void countElements(List<int> liste){
+    private void countElements(List<int> liste){
         Console.WriteLine("The number of elements in the list is: " + liste.Count);
     }
-    public void viderListe(List<int> liste){
+    private void viderListe(List<int> liste){
         liste.Clear();
         Console.WriteLine("The list is empty");
     }
 
-    public void getElement(int id){
+    private void getElement(int id){
         int element = liste[id];
         if(id < liste.Count && id >= 0){
             Console.WriteLine("Element with the id " + id + " is: " + element);
